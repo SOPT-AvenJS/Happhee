861072b6354612f29c361c8d7e7daf8303a47f66
seohee0112 <ghdtj7@naver.com>
2022-06-30 17:52:55 +0900

    [ feat ] 게임 시작, 단어 받아오기, 단어 길이 예외처리, 키보드로 단어 입력받기

---
+++ js/index.js
@@ -0,0 +1,57 @@
+const $ = (selector) => document.querySelector(selector);
+const $$ = (selector) => document.querySelectorAll(selector);
+let answerWord;
+let wordSection = $('.game__word');
+let restChance = $('.chance__rest');
+
+document.addEventListener('keypress', (event) => {
+  const alphabet = String.fromCharCode(event.keyCode).toUpperCase();
+
+  if (answerWord.indexOf(alphabet) > -1) {
+    const currentWord = wordSection.innerHTML.split(' ').filter((alphabet) => alphabet !== '');
+    wordSection.innerHTML = '';
+    answerWord.forEach((answerAlphabet, idx) => {
+      if (currentWord[idx] !== '_') wordSection.innerHTML += ` ${currentWord[idx]} `;
+      else if (answerAlphabet === alphabet) wordSection.innerHTML += ` ${alphabet} `;
+      else wordSection.innerHTML += ' _ ';
+    });
+  } else {
+  }
+});
+
+// 단어 받아오기
+const getWord = async () => {
+  const response = await fetch('http://puzzle.mead.io/puzzle?wordCount=1');
+  if (response.status === 200) {
+    const data = await response.json();
+    return data.puzzle.toUpperCase().split('');
+  } else {
+    throw new Error('unable to get puzzle');
+  }
+};
+// 단어 길이 예외
+const isWordLength = (word) => {
+  return word.length !== 6 ? true : false;
+};
+
+const attachEvent = (gameButton) => {
+  gameButton.addEventListener('click', initGame);
+};
+// 게임 시작 / 재시작
+const initGame = async () => {
+  answerWord = await getWord();
+
+  while (isWordLength(answerWord)) {
+    answerWord = await getWord();
+  }
+  wordSection.innerHTML = '';
+  restChance.innerHTML = answerWord.length + 3;
+  answerWord.forEach(() => {
+    wordSection.innerHTML += ' _ ';
+  });
+  console.log(answerWord);
+};
+window.onload = () => {
+  initGame();
+  attachEvent($('.game__button'));
+};
